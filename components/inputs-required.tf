5aa8c0f5ffe32ff5431c0d906ac53053cf054310
affableaussie <216882833+affableaussie@users.noreply.github.com>
2026-06-02 10:17:59 +0100

    fix: adding the correct variable files

--- components/inputs-required.tf
+++ components/inputs-required.tf
@@ -9,3 +9,16 @@ variable "builtFrom" {
 variable "product" {
   type = string
 }
+
+variable "environment" {}
+variable "oms_env" {}
+variable "private_ip_address" {}
+variable "project" {}
+variable "subscription" {}
+variable "vnet_name" {}
+variable "vnet_rg" {}
+variable "ssl_certificate" {}
+
+# Declared to satisfy the shared tfvars file; not used in this component
+variable "destinations" {}
+variable "frontends" {}
