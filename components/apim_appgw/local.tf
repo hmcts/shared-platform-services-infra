32471ff2d9e561e69a93b07a253cf4d4ab61abcd
affableaussie <216882833+affableaussie@users.noreply.github.com>
2026-06-05 11:39:54 +0100

    fix: vnet_rg and vnet_name logic

--- components/apim_appgw/local.tf
+++ components/apim_appgw/local.tf
@@ -1,9 +1,9 @@
 locals {
   env = var.env
 
-  vnet_rg     = var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "test" ? "hmcts-hub-nonprodi" : (var.env == "stg") ? "hmcts-hub-prod-int" : "hmcts-hub-${var.env}-int"
-  vnet_name   = var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "test" ? "hmcts-hub-nonprodi" : (var.env == "stg") ? "hmcts-hub-prod-int" : "hmcts-hub-${var.env}-int"
-  subnet_name = "hub-appgw"
+  vnet_rg     = (var.env == "dev") ? "rg-sps-platform-preview" : (var.env == "test") ? "rg-sps-platform-perftest" : (var.env == "stg") ? "rg-sps-platform-aat" : "rg-sps-platform-${var.env}"
+  vnet_name   = (var.env == "dev") ? "sps-platform-networking-vnet-preview" : (var.env == "test") ? "rsps-platform-networking-vnet-perftest" : (var.env == "stg") ? "sps-platform-networking-vnet-aat" : "sps-platform-networking-vnet-${var.env}"
+  subnet_name = "sps-platform-networking-app-gateway-sbox"
   hub_env     = (var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "test") ? "nonprod" : (var.env == "stg") ? "prod" : var.env
 
   key_vault_resource_group = "sps-platform-${var.subscription}-rg"
