932e40b4fd07a234dfca217c5a765d5c4032a0a1
affableaussie <216882833+affableaussie@users.noreply.github.com>
2026-06-05 12:52:59 +0100

    fix: local{}

--- components/apim_appgw/local.tf
+++ components/apim_appgw/local.tf
@@ -16,28 +16,28 @@ locals {
   }
 
 
-#   hub = {
-#     sbox = {
-#       subscription = "ea3a8c1e-af9d-4108-bc86-a7e2d267f49c"
-#       ukSouth = {
-#         name              = "hmcts-hub-sbox-int"
-#         next_hop_ip       = "10.10.200.36"
-#         appgw_next_hop_ip = "20.90.242.134" # Azure Firewall
-#       }
-#     }
-#     nonprod = {
-#       subscription = "fb084706-583f-4c9a-bdab-949aac66ba5c"
-#       ukSouth = {
-#         name        = "hmcts-hub-nonprodi"
-#         next_hop_ip = "10.11.72.36"
-#       }
-#     }
-#     prod = {
-#       subscription = "0978315c-75fe-4ada-9d11-1eb5e0e0b214"
-#       ukSouth = {
-#         name        = "hmcts-hub-prod-int"
-#         next_hop_ip = "10.11.8.36"
-#       }
-#     }
-#   }
- }
+  hub = {
+    sbox = {
+      subscription = "ea3a8c1e-af9d-4108-bc86-a7e2d267f49c"
+      ukSouth = {
+        name              = "hmcts-hub-sbox-int"
+        next_hop_ip       = "10.10.200.36"
+        appgw_next_hop_ip = "20.90.242.134" # Azure Firewall
+      }
+    }
+    nonprod = {
+      subscription = "fb084706-583f-4c9a-bdab-949aac66ba5c"
+      ukSouth = {
+        name        = "hmcts-hub-nonprodi"
+        next_hop_ip = "10.11.72.36"
+      }
+    }
+    prod = {
+      subscription = "0978315c-75fe-4ada-9d11-1eb5e0e0b214"
+      ukSouth = {
+        name        = "hmcts-hub-prod-int"
+        next_hop_ip = "10.11.8.36"
+      }
+    }
+  }
+}
