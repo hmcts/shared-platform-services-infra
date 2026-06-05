d3d021b36240e0ce3697a5079d42d03d324faccf
Ciaran Carragher <ciaranca@kainos.com>
2026-06-05 13:11:00 +0100

    use platform VNet and subnet for apim_appgw and add core dependency

--- components/apim_appgw/local.tf
+++ components/apim_appgw/local.tf
@@ -1,9 +1,9 @@
 locals {
   env = var.env
 
-  vnet_rg     = var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "test" ? "hmcts-hub-nonprodi" : (var.env == "stg") ? "hmcts-hub-prod-int" : "hmcts-hub-${var.env}-int"
-  vnet_name   = var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "test" ? "hmcts-hub-nonprodi" : (var.env == "stg") ? "hmcts-hub-prod-int" : "hmcts-hub-${var.env}-int"
-  subnet_name = "hub-appgw"
+  vnet_rg     = "rg-${var.product}-${var.env}"
+  vnet_name   = "${var.product}-networking-vnet-${var.env}"
+  subnet_name = "${var.product}-networking-app-gateway-${var.env}"
   hub_env     = (var.env == "demo" || var.env == "dev" || var.env == "ithc" || var.env == "test") ? "nonprod" : (var.env == "stg") ? "prod" : var.env
 
   key_vault_resource_group = "sps-platform-${var.subscription}-rg"
