bd9675d49bdc2e50563431dd19396bf18e3faa12
affableaussie <216882833+affableaussie@users.noreply.github.com>
2026-06-08 11:05:01 +0100

    feature: adding apimm code

---
+++ components/apim/init.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.13.3, < 1.14.9"
+
+  backend "azurerm" {
+  }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
---
+++ components/apim/main.tf
@@ -0,0 +1,30 @@
+module "ctags" {
+  source       = "git::https://github.com/hmcts/terraform-module-common-tags.git?ref=master"
+  environment  = var.env
+  product      = var.product
+  builtFrom    = var.builtFrom
+  expiresAfter = var.expiresAfter
+}
+
+module "api-mgmt" {
+  source                               = "git::https://github.com/hmcts/cnp-module-api-mgmt-private.git?ref=main"
+  location                             = var.location
+  sku_name                             = var.apim_sku_name
+  virtual_network_resource_group       = local.vnet_rg
+  virtual_network_name                 = local.vnet_name
+  environment                          = var.env
+  virtual_network_type                 = "Internal"
+  department                           = var.department
+  common_tags                          = module.ctags.common_tags
+  route_next_hop_in_ip_address         = local.hub[var.hub].ukSouth.next_hop_ip
+  publisher_email                      = var.publisher_email
+  disable_trusted_service_connectivity = var.disable_trusted_service_connectivity
+}
+
+resource "azurerm_api_management_named_value" "environment" {
+  name                = "environment"
+  resource_group_name = local.vnet_rg
+  api_management_name = module.api-mgmt.name
+  display_name        = "environment"
+  value               = var.env
+}
