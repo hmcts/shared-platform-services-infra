967cafbecbce87c5997bb126e34f6729f02aec59
affableaussie <216882833+affableaussie@users.noreply.github.com>
2026-06-26 15:01:02 +0100

    updating variables

--- components/core/cross-tenant-peering.tf
+++ components/core/cross-tenant-peering.tf
@@ -25,6 +25,7 @@ data "azurerm_key_vault_secret" "multi_tenant_client_secret" {
 provider "azurerm" {
   alias                      = "central-app-kv"
   features {}
+  skip_provider_registration = true
   subscription_id            = "6c4d2513-a873-41b4-afdd-b05a33206631" # Central App Registration subscription
 }
 
