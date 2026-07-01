3250638a256fa1fbdf432ae3548dfe52d2a7fc1e
Alex Bance <Alex.Bance@justice.gov.uk>
2026-07-01 13:30:45 +0100

    fix: correct import resource id

--- components/apim/imports.tf
+++ components/apim/imports.tf
@@ -5,7 +5,7 @@ import {
 
 import {
   to = module.api-mgmt.azurerm_api_management_custom_domain.api-management-custom-domain
-  id = "/subscriptions/bd2864ed-4f3e-45ed-9c6a-8d179674bab1/resourceGroups/rg-sps-platform-sbox/providers/Microsoft.ApiManagement/service/sps-api-mgmt-sbox"
+  id = "/subscriptions/bd2864ed-4f3e-45ed-9c6a-8d179674bab1/resourceGroups/rg-sps-platform-sbox/providers/Microsoft.ApiManagement/service/sps-api-mgmt-sbox/customDomains/default"
 }
 
 import {
