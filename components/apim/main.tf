f6a7fa5aa9faf7f0744f7e7c5ec22c942eb82534
Alex Bance <Alex.Bance@justice.gov.uk>
2026-08-20 13:09:58 +0100

    fix: override key vault environment

--- components/apim/main.tf
+++ components/apim/main.tf
@@ -40,6 +40,7 @@ module "api-mgmt" {
   apim_diagnostic_settings = var.apim_diagnostic_settings
   acme_environment         = var.env == "dev" ? "preview" : var.env == "test" ? "perftest" : null
   acme_rg_name             = contains(["dev", "test"], var.env) ? "sps-platform-${var.env}-rg" : null
+  key_vault_environment    = var.env == "dev" ? "preview" : var.env == "test" ? "perftest" : null
 }
 
 resource "azurerm_api_management_named_value" "environment" {
