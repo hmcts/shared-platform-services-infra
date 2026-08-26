b7d5814ab5e29efd52a275d24cb6515293315722
Alex Bance <Alex.Bance@justice.gov.uk>
2026-08-26 16:07:32 +0100

    fix: correct staging kv reference

--- components/apim_appgw/local.tf
+++ components/apim_appgw/local.tf
@@ -2,7 +2,7 @@ locals {
 
   key_vault_resource_group = "sps-platform-${var.env}-rg"
   key_vault_subscription   = var.key_vault_subscription
-  key_vault_name           = "acmedtssps${var.subscription}"
+  key_vault_name           = "acmedtssps${var.subscription == "aat" ? "stg" : var.subscription}"
   dns_zone                 = (var.env == "sbox") ? "sandbox" : var.env
 
   # To be removed once the TLS1.0/1.1 deprecation is complete
