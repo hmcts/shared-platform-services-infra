43d3aa1c640c016221072b53a9aa9ed945c83e3b
jordankainos <j.brown@kainos.com>
2026-09-08 14:34:00 +0100

    Update main.tf

--- components/core/main.tf
+++ components/core/main.tf
@@ -1,7 +1,9 @@
 locals {
   naming_env = var.env == "dev" ? "preview" : var.env == "test" ? "perftest" : var.env == "stg" ? "aat" : var.env
 
-  api_marketplace_apim_key_reader_envs = toset(["dev"])
+  api_marketplace_reader_envs      = toset(["dev", "test", "stg"])
+  api_marketplace_custom_role_envs = toset(["dev", "test"])
+  api_marketplace_contributor_envs = toset(["sbox"])
 
   # azure-private-dns uses a different service connection in sandbox.
   private_dns_pipeline_principal_ids = var.env == "sbox" ? toset([
@@ -11,19 +13,58 @@ locals {
     "b8f08f77-4ce2-43d5-a23b-c7ca735eca02"
   ])
 
-  role_assignments = merge(var.env != "prod" ? {
-    "api_marketplace-apim" = {
-      scope                = azurerm_resource_group.this.id
-      role_definition_name = contains(local.api_marketplace_apim_key_reader_envs, var.env) ? "APIM Subscription Key Reader" : "Reader"
-      principal_id         = data.azuread_group.api_marketplace.object_id
-    }
-    } : {}, var.deploy_extid_rg ? {
-    "api_marketplace-extid" = {
-      scope                = azurerm_resource_group.extid[0].id
-      role_definition_name = contains(local.api_marketplace_apim_key_reader_envs, var.env) ? "APIM Subscription Key Reader" : "Reader"
-      principal_id         = data.azuread_group.api_marketplace.object_id
-    }
-  } : {})
+  role_assignments = merge(
+    # Contributor in sbox only
+    (var.env != "prod" && contains(local.api_marketplace_contributor_envs, var.env)) ? {
+      "api_marketplace-apim-contributor" = {
+        scope                = azurerm_resource_group.this.id
+        role_definition_name = "Contributor"
+        principal_id         = data.azuread_group.api_marketplace.object_id
+      }
+    } : {},
+
+    (var.deploy_extid_rg && var.env != "prod" && contains(local.api_marketplace_contributor_envs, var.env)) ? {
+      "api_marketplace-extid-contributor" = {
+        scope                = azurerm_resource_group.extid[0].id
+        role_definition_name = "Contributor"
+        principal_id         = data.azuread_group.api_marketplace.object_id
+      }
+    } : {},
+
+    # Reader in dev/test/stg (not sbox)
+    (var.env != "prod" && contains(local.api_marketplace_reader_envs, var.env)) ? {
+      "api_marketplace-apim-reader" = {
+        scope                = azurerm_resource_group.this.id
+        role_definition_name = "Reader"
+        principal_id         = data.azuread_group.api_marketplace.object_id
+      }
+    } : {},
+
+    (var.deploy_extid_rg && var.env != "prod" && contains(local.api_marketplace_reader_envs, var.env)) ? {
+      "api_marketplace-extid-reader" = {
+        scope                = azurerm_resource_group.extid[0].id
+        role_definition_name = "Reader"
+        principal_id         = data.azuread_group.api_marketplace.object_id
+      }
+    } : {},
+
+    # Extra custom role in dev/test only
+    (var.env != "prod" && contains(local.api_marketplace_custom_role_envs, var.env)) ? {
+      "api_marketplace-apim-sub-key-reader" = {
+        scope                = azurerm_resource_group.this.id
+        role_definition_name = "APIM Subscription Key Reader"
+        principal_id         = data.azuread_group.api_marketplace.object_id
+      }
+    } : {},
+
+    (var.deploy_extid_rg && var.env != "prod" && contains(local.api_marketplace_custom_role_envs, var.env)) ? {
+      "api_marketplace-extid-sub-key-reader" = {
+        scope                = azurerm_resource_group.extid[0].id
+        role_definition_name = "APIM Subscription Key Reader"
+        principal_id         = data.azuread_group.api_marketplace.object_id
+      }
+    } : {}
+  )
 }
 
 resource "azurerm_resource_group" "this" {
